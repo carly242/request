41a941f11c8d58d390b9f4b41b076216863b4833
carly242 <carlybivihou8@gmail.com>
2024-06-24 03:46:25 +0200

    path

--- Kilos/settings.py
+++ Kilos/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'Kilos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'Templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
